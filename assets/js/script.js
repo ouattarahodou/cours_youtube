d071a2ef5ed99bb12875b86fdb19be3d6ebe057c
ouattarahodou <83280873+ouattarahodou@users.noreply.github.com>
2022-03-16 12:10:40 +0100

    ajout des fichiers js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 window.onload = () =>{
     var collapse = document.getElementById("collapse")
     var menu = document.getElementById("menu")
+    var listes = document.querySelectorAll(".header-left ul li")
 
     if(collapse) {
         collapse.addEventListener("click", ()=>{
@@ -18,6 +19,11 @@ window.onload = () =>{
             menu.addEventListener("mouseleave", ()=>{
                 menu.style.display = "none"
             })
+            for (let index = 0; index < listes.length; index++) {
+                const li = listes[index];
+                li.onclick = listenerFunction.closeNav
+                
+            }
         }
     }
 }
